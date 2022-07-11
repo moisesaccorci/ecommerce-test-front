28480632964b20a4564f5af63c5fe0843d26ef79
Moises Accorci de Mattos <moisesaccorci@gmail.com>
2022-07-11 08:07:32 -0300

    config vars not working on heroku, using plain text

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: process.env.API_URL,
+    baseURL: "https://test-ecommerce-api-ma.herokuapp.com",
     headers: {
         "Content-Type": "Authorization",
         "Content-Type": "application/json",
